d0eb86557dfe72e7beee67c83b0ee8fc6b0c279f
Thanakorn Saengprachum <ThanaSaengprachum@hotmail.com>
2024-05-30 21:17:50 +0200

    newsletter fixed

--- NewsletterProvider/Functions/Subscribe.cs
+++ NewsletterProvider/Functions/Subscribe.cs
@@ -6,6 +6,10 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Azure.Messaging.ServiceBus;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace NewsletterProvider.Functions
 {
@@ -13,11 +17,13 @@ namespace NewsletterProvider.Functions
     {
         private readonly ILogger<Subscribe> _logger;
         private readonly DataContext _context;
+        private readonly ServiceBusClient _serviceBusClient;
 
-        public Subscribe(ILogger<Subscribe> logger, DataContext context)
+        public Subscribe(ILogger<Subscribe> logger, DataContext context, ServiceBusClient serviceBusClient)
         {
             _logger = logger;
             _context = context;
+            _serviceBusClient = serviceBusClient;
         }
 
         [Function("Subscribe")]
@@ -29,22 +35,57 @@ namespace NewsletterProvider.Functions
                 var subscribeEntity = JsonConvert.DeserializeObject<SubsribeEntity>(body);
                 if (subscribeEntity != null)
                 {
-
                     var existingSubscriber = await _context.Subscribers.FirstOrDefaultAsync(x => x.Email == subscribeEntity.Email);
                     if (existingSubscriber != null)
                     {
                         _context.Entry(existingSubscriber).CurrentValues.SetValues(subscribeEntity);
-                        await _context.SaveChangesAsync();  
-                        return new OkObjectResult(new { Status = 200, Message = "Subscriber Updated"});
-
+                        await _context.SaveChangesAsync();
+                        await SendEmailRequest(subscribeEntity);
+                        return new OkObjectResult(new { Status = 200, Message = "Subscriber Updated" });
                     }
 
                     _context.Subscribers.Add(subscribeEntity);
                     await _context.SaveChangesAsync();
+                    await SendEmailRequest(subscribeEntity);
                     return new OkObjectResult(new { Status = 200, Message = "Subscriber added" });
                 }
             }
             return new BadRequestObjectResult(new { Status = 400, Message = "Unable to subscribe" });
+        }
+
+        private async Task SendEmailRequest(SubsribeEntity subscribeEntity)
+        {
+            var subscriptionDetails = new StringBuilder();
+            subscriptionDetails.Append("<html><p>You are now subscribed to:</p><ul>");
+
+            if (subscribeEntity.DailyNewsletter)
+                subscriptionDetails.Append("<li>Daily Newsletter</li>");
+            if (subscribeEntity.AdvertisingUpdates)
+                subscriptionDetails.Append("<li>Advertising Updates</li>");
+            if (subscribeEntity.WeekinReview)
+                subscriptionDetails.Append("<li>Week in Review</li>");
+            if (subscribeEntity.EventUpdates)
+                subscriptionDetails.Append("<li>Event Updates</li>");
+            if (subscribeEntity.StartupWeekly)
+                subscriptionDetails.Append("<li>Startup Weekly</li>");
+            if (subscribeEntity.Podcasts)
+                subscriptionDetails.Append("<li>Podcasts</li>");
+
+            subscriptionDetails.Append("</ul></html>");
+
+            var emailContent = new
+            {
+                RecipientAddress = subscribeEntity.Email,
+                Subject = "Subscription Confirmation",
+                HtmlContent = subscriptionDetails.ToString(),
+                PlainTextContent = "You are now subscribed to our newsletters."
+            };
+
+            var messageBody = JsonConvert.SerializeObject(emailContent);
+            var message = new ServiceBusMessage(Encoding.UTF8.GetBytes(messageBody));
+
+            ServiceBusSender sender = _serviceBusClient.CreateSender("email_request");
+            await sender.SendMessageAsync(message);
         }
     }
 }
--- NewsletterProvider/Program.cs
+++ NewsletterProvider/Program.cs
@@ -1,17 +1,28 @@
-using Data.Contexts;
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+    using Azure.Messaging.ServiceBus;
+    using Data.Contexts;
+    using Microsoft.Azure.Functions.Worker;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services =>
-    {
-        services.AddApplicationInsightsTelemetryWorkerService();
-        services.ConfigureFunctionsApplicationInsights();
-        services.AddDbContext<DataContext>(x => x.UseSqlServer(Environment.GetEnvironmentVariable("SqlServer")));
-    })
-    .Build();
+    var host = new HostBuilder()
+        .ConfigureFunctionsWebApplication()
+        .ConfigureServices(services =>
+        {
+            services.AddApplicationInsightsTelemetryWorkerService();
+            services.ConfigureFunctionsApplicationInsights();
 
-host.Run();
+            var sqlServerConnectionString = Environment.GetEnvironmentVariable("SqlServer");
+            // Register the DbContext with the retrieved connection string
+            services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(sqlServerConnectionString));
+
+            // Retrieve the Service Bus connection string from the environment variables
+            var serviceBusConnectionString = Environment.GetEnvironmentVariable("ServiceBusConnection");
+
+            // Register the ServiceBusClient with the retrieved connection string
+            services.AddSingleton(new ServiceBusClient(serviceBusConnectionString));
+        })
+        .Build();
+
+    host.Run();
